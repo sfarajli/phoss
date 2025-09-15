cc749a928007403c33bfce40a59c5046258f59a4
Suleyman Farajli <suleyman@farajli.net>
2025-09-15 14:45:21 +0400

    feat(server): queue implemented

--- lib/server.c
+++ lib/server.c
@@ -8,17 +8,99 @@
 #include <nng/nng.h>
 #include <nng/protocol/pubsub0/pub.h>
 
-/* TODO: implement queue data structure */
+#define REQUEST_LIMIT 100
 
+typedef struct {
+	cJSON *items[REQUEST_LIMIT];
+	int front;
+	int rear;
+	int size;
+} JSONQueue;
+
+static JSONQueue *request_queue = NULL;
 static char *link = NULL;
 
 bool server_run(const char *ip, const int port, const char *pl);
-bool _server_close();
-bool _interpreter_close();
+bool _server_close(void);
+bool _interpreter_close(void);
 bool _interpreter_start(const char *pl);
-bool _request_handle();
+bool _request_handle(void);
 char *_get_link(const char *ip, const int port);
-void _queue_handle();
+void _queue_handle(void);
+
+void _request_queue_init(void);
+void _request_queue_free(void);
+bool _request_queue_isempty(void);
+bool _request_queue_isfull(void);
+void _request_enqueue(cJSON *request_json);
+cJSON *_request_dequeue(void);
+cJSON *_request_queue_peek(void);
+
+bool
+_request_queue_init(void)
+{
+	request_queue = (JSONQueue*)malloc(sizeof(JSONQueue));
+	if (!request_queue) {
+		return false;
+	}
+
+	request_queue->front = 0;
+	request_queue->rear = -1;
+	request_queue->size = 0;
+
+	return true;
+}
+
+void
+_request_queue_free()
+{
+	while (!request_queue_isempty())
+		cJSON_Delete(_request_dequeue);
+}
+
+bool
+_request_queue_isfull(void)
+{
+	return request_queue->size == MAX_QUEUE_SIZE;
+}
+
+bool
+_request_queue_isempty(void)
+{
+	return request_queue->size == 0;
+}
+
+void
+_request_enqueue(cJSON *request_json)
+{
+	if (request_queue_isfull())
+		return NULL;
+
+	request_queue->rear = (request_queue->rear + 1) % REQUEST_LIMIT;
+	request_queue->items[request_queue->rear] = request_json;
+	request_queue->size++;
+}
+
+cJSON *
+_request_dequeue(void)
+{
+	if (request_queue_isfull())
+		return NULL;
+
+	cJSON *request_json= request_queue->items[request_queue->front];
+	request_queue->front = (request_queue->front + 1) % REQUEST_LIMIT;
+	request_queue->size--;
+	return request_json;
+}
+
+cJSON *
+_request_queue_peek(void)
+{
+    	if (request_queue_isempty())
+        	return NULL;
+
+	return request_queue->items[request_queue->front];
+}
 
 char *
 _get_link(char *ip, int port)
