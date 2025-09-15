795d1f5348d8461191d776b39e26555822601544
Suleyman Farajli <suleyman@farajli.net>
2025-09-15 11:26:20 +0400

    chore: update todo comments

--- lib/server.c
+++ lib/server.c
@@ -8,6 +8,8 @@
 #include <nng/nng.h>
 #include <nng/protocol/pubsub0/pub.h>
 
+/* TODO: implement queue data structure */
+
 static char *link = NULL;
 
 bool server_run(const char *ip, const int port, const char *pl);
@@ -38,9 +40,12 @@ _get_link(char *ip, int port)
 void
 _queue_handle()
 {
+	/* Initialize request queue */
 	/* Listen to the given link */
-	/* Store all the JSON requests inside of a queue structure */
+	/* Store all the JSON requests inside of the queue */
+	/* If the number of requests exceed the limit give a warning */
 	/* Call request_handle one by one for each request in queue */
+	/* Do not handle the next request unless the current one is completely finished */
 	/* Cancel all the following requests if one request fails */
 }
 
